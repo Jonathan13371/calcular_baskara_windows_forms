9a1a1d0c4efb398ad26e5cfb7fa8cf026bf63de1
Jonathan <Jonathan@teste.com>
2024-03-04 15:49:51 -0300

    sexto exercio windows form

---
+++ ex07/calcular_baskara/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace ex07
+{
+    partial class frmTelaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.textA = new System.Windows.Forms.TextBox();
+            this.textB = new System.Windows.Forms.TextBox();
+            this.textC = new System.Windows.Forms.TextBox();
+            this.bntCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSaida = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(324, 73);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(22, 16);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "A:";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(324, 110);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(22, 16);
+            this.lblB.TabIndex = 1;
+            this.lblB.Text = "B:";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Location = new System.Drawing.Point(324, 155);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(22, 16);
+            this.lblC.TabIndex = 2;
+            this.lblC.Text = "C:";
+            this.lblC.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textA
+            // 
+            this.textA.Location = new System.Drawing.Point(364, 70);
+            this.textA.Name = "textA";
+            this.textA.Size = new System.Drawing.Size(100, 22);
+            this.textA.TabIndex = 3;
+            // 
+            // textB
+            // 
+            this.textB.Location = new System.Drawing.Point(364, 110);
+            this.textB.Name = "textB";
+            this.textB.Size = new System.Drawing.Size(100, 22);
+            this.textB.TabIndex = 4;
+            // 
+            // textC
+            // 
+            this.textC.Location = new System.Drawing.Point(364, 149);
+            this.textC.Name = "textC";
+            this.textC.Size = new System.Drawing.Size(100, 22);
+            this.textC.TabIndex = 5;
+            // 
+            // bntCalcular
+            // 
+            this.bntCalcular.Location = new System.Drawing.Point(364, 197);
+            this.bntCalcular.Name = "bntCalcular";
+            this.bntCalcular.Size = new System.Drawing.Size(100, 53);
+            this.bntCalcular.TabIndex = 6;
+            this.bntCalcular.Text = "Calcular Baskara";
+            this.bntCalcular.UseVisualStyleBackColor = true;
+            this.bntCalcular.Click += new System.EventHandler(this.bntCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(316, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Resposta:";
+            // 
+            // lblSaida
+            // 
+            this.lblSaida.AutoSize = true;
+            this.lblSaida.Location = new System.Drawing.Point(402, 279);
+            this.lblSaida.Name = "lblSaida";
+            this.lblSaida.Size = new System.Drawing.Size(0, 16);
+            this.lblSaida.TabIndex = 8;
+            // 
+            // frmTelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1084, 504);
+            this.Controls.Add(this.lblSaida);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bntCalcular);
+            this.Controls.Add(this.textC);
+            this.Controls.Add(this.textB);
+            this.Controls.Add(this.textA);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmTelaPrincipal";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.TextBox textA;
+        private System.Windows.Forms.TextBox textB;
+        private System.Windows.Forms.TextBox textC;
+        private System.Windows.Forms.Button bntCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSaida;
+    }
+}
+
---
+++ ex07/calcular_baskara/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex07
+{
+    public partial class frmTelaPrincipal : Form
+    {
+        public frmTelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bntCalcular_Click(object sender, EventArgs e)
+        {
+
+            double a, b, c, x1, x2;
+
+            a = Convert.ToDouble(textA.Text.Replace('.', ','));
+            b = Convert.ToDouble(textB.Text.Replace('.', ','));
+            c = Convert.ToDouble(textC.Text.Replace('.', ','));
+
+            x1 = (-b + Math.Sqrt((b * b) - (4 * a * c))) / (2 * a);
+
+            x2 = (-b - Math.Sqrt((b * b) - (4 * a * c))) / (2 * a);
+
+            lblSaida.Text = "X1 = " + x1 + "\r\n" +
+                           "X2 = " + x2;
+        }
+    }
+}
